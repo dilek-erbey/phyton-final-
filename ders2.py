dca30bf17e1021eddff1fd2cb6ae95204216af7e
dilek-erbey <56965508+dilek-erbey@users.noreply.github.com>
2021-02-09 13:50:06 +0300

    Add files via upload

---
+++ ders2.py
@@ -0,0 +1,12 @@
+"""print("elma","armut","portakal",sep="-")
+print("Gazi","antep",sep="+")
+isim=input("adnızı yazınız?\n")"""
+#print("bir","iki",end=" çiçek")
+
+yas=int(input("yasınızı yazınız.\n"))
+if yas<15:
+    print("çocuk")
+elif yas<40:
+    print("genç")
+else:
+    print("yetişkin")
