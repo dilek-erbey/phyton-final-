e94ddc33f7d435e9d3784a1088007c9893fc04c7
dilek-erbey <56965508+dilek-erbey@users.noreply.github.com>
2021-02-09 13:51:15 +0300

    Add files via upload

---
+++ ders5listeler.py
@@ -0,0 +1,11 @@
+"""sayi=int(input("Tuttuğum sayı sence hangisi?\n\t"))
+liste=[8,6,45,90,"phyton",34,23,45]
+for sayilar in liste:
+    if sayi in liste:
+        print("buldunuz")
+    else:
+        print("Bulamadınız.Tekrar bir sayı yazınız")"""""
+meyveler=["elma","armut","kivi"]
+print(meyveler)
+print(meyveler[0])
+
