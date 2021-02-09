e9419deedc4043ca890fcaf76fcd20eb4d236657
dilek-erbey <56965508+dilek-erbey@users.noreply.github.com>
2021-02-10 01:56:20 +0300

    Add files via upload

---
+++ Ders11bilimsel hesap makinesi.py
@@ -0,0 +1,16 @@
+import math as ma
+pi=ma.pi()
+cos=ma.cos()
+rad=ma.radians()
+log=ma.log10()
+kare=ma.sqrt()
+x=1
+print(".>çıkış için "exit"\n" .>komutlar için"komut"")
+print()
+while x!=="exit":
+    x=eval(input(">>"))
+    if x== "help":
+        print(p= pisayısı\n cos:kosinüs\n lg:logaritma\n rad: radyan\nkare:karesinial)
+    else:
+        print(x)
+
